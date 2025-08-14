7141cc8d580b2685d45ff36fee5be4b687069b70
Llogari Casas <llogari92@gmail.com>
2025-08-14 16:23:07 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FocusEntity",
-    platforms: [.iOS(.v18)],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         .library(name: "FocusEntity", targets: ["FocusEntity"])
     ],
